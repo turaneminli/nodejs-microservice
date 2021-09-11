e03a66e324b6697b3900ad7526d052ece8b72a13
turaneminli <eminli.turan@gmail.com>
2021-09-11 23:36:53 +0400

    moderation service for comments added and client side updated for status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,9 +2,20 @@
 import React from "react";
 
 export default ({ comments }) => {
-  console.log("Comments:" + comments);
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "Please wait, it is in moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
